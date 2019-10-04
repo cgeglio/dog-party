5314ff0eb930e9ffcb95e0a6ac1e2b42fad2ff54
Carla Geglio <carla.geglio@gmail.com>
2019-10-03 21:29:14 -0600

    Add functionality to dog name form

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-function dogName(doggo) {
-  return 
+window.onload = function() {
+  var span = document.querySelector("span")
+  var form = document.querySelector("form");
+  form.addEventListener("submit", function(event) {
+    span.innerText = (form.elements.value.value);
+    event.preventDefault();
+  });
+}
